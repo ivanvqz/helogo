1b0467ea556e34ced58cf4248da77d21e1619716
Ivan <ivanvqz103@gmail.com>
2022-10-28 16:35:38 -0500

    yes

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
-import "fmt"
 
-func main(){
-	fmt.Println("Hola mundo")
+import (
+	"fmt"
+
+	quoteV3 "rsc.io/quote/v3"
+
+	"github.com/ivanvqz/helogo/greet"
+
+)
+
+func main() {
+	fmt.Println(greet.Japanese())
+	fmt.Println(quoteV3.HelloV3())
+	fmt.Println(quoteV3.Concurrency())
 }
